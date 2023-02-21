bbcdce4980dd6bbace2b679a33f195dc0441fe04
unknown <ammarasif509@gmail.com>
2023-02-21 19:46:45 +0500

    Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,12 @@ import "./css/style.css";
 import Header from "./pages/Header";
 import Landing from "./pages/Landing";
 import { useNavigate } from "react-router-dom";
+import Sidebar from "./components/Sidebar/Sidebar";
 function App() {
   const navigate = useNavigate();
   const user = localStorage.getItem("User");
+  const [sidebarOpen, setSidebarOpen] = React.useState(false);
+
   React.useEffect(() => {
     if (!user) {
       navigate("/login");
@@ -16,8 +19,18 @@ function App() {
   return (
     <>
       <div className="flex h-screen overflow-hidden">
+        {user ? (
+          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        ) : (
+          <></>
+        )}
         <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-          {user ? <Header /> : <></>}
+          {user ? (
+            <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          ) : (
+            <></>
+          )}
+
           <Routes>
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/" element={<Landing />} />
