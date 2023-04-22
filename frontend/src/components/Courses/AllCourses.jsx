6754e5758b8ad066ab12330585e77b0b4df0db04
RafayFarrukh <rafayfarrukh941@gmail.com>
2023-04-22 22:16:21 +0500

    length error

--- frontend/src/components/Courses/AllCourses.jsx
+++ frontend/src/components/Courses/AllCourses.jsx
@@ -1,63 +1,63 @@
-import React, { useState, useEffect } from "react";
-import axiosInstance from "../../services/axiosInstance";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import axiosInstance from '../../services/axiosInstance';
+import { useNavigate } from 'react-router-dom';
 
-import axios from "axios";
+import axios from 'axios';
 
 const AllCourses = () => {
   const navigate = useNavigate();
   const [courses, setCourses] = useState([]);
   const [singlecourse, setSinglecourse] = useState();
-  const token = localStorage.getItem("Token");
+  const token = localStorage.getItem('Token');
   useEffect(() => {
     // console.log(courses._id);
-  }, []);  
+  }, []);
 
   useEffect(() => {
-    axiosInstance.get("api/course/teacher/ShowAllCourses").then((res) => {
-       setSinglecourse(res.data.courseList?.map((course) => course._id))
+    axiosInstance.get('api/course/teacher/ShowAllCourses').then((res) => {
+      setSinglecourse(res.data.courseList?.map((course) => course._id));
       // console.log(courseList.map((course) => course.Course));
       setCourses(res.data.courseList?.map((course) => course.Course));
 
-    //   console.log(res.data.courseList.map((course) => course.Course));
+      //   console.log(res.data.courseList.map((course) => course.Course));
       // console.log(courses);
     });
   }, [courses]);
   return (
     <>
-      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700  bg-slate-900 dark:bg-gray-700 dark:text-gray-400">
+      <table class='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
+        <thead class='text-xs text-gray-700  bg-slate-900 dark:bg-gray-700 dark:text-gray-400'>
           <tr>
-            <th scope="col" class="py-3 px-6">
+            <th scope='col' class='py-3 px-6'>
               Course Code
             </th>
-            <th scope="col" class="py-3 px-6">
+            <th scope='col' class='py-3 px-6'>
               CourseName
             </th>
-            <th scope="col" class="py-3 px-6">
+            <th scope='col' class='py-3 px-6'>
               CourseShortNam
             </th>
-             <th scope="col" class="py-3 px-6">
+            <th scope='col' class='py-3 px-6'>
               Details
             </th>
           </tr>
         </thead>
         <tbody>
-          {courses.length >= 0 ? (
-            courses.map((course, key) => (
+          {courses?.length >= 0 ? (
+            courses?.map((course, key) => (
               <tr key={course._id}>
-                <td className="py-4 px-6">{course._id.courseCode}</td>
-                <td className="py-4 px-6">{course._id.courseName}</td>
-                <td className="py-4 px-6">{course._id.courseShortName}</td>
+                <td className='py-4 px-6'>{course._id.courseCode}</td>
+                <td className='py-4 px-6'>{course._id.courseName}</td>
+                <td className='py-4 px-6'>{course._id.courseShortName}</td>
                 {/* <td className="py-4 px-6">{course._id}</td> */}
 
-                <td className="flex py-4 px-6">
+                <td className='flex py-4 px-6'>
                   <button
-                    type="submit"
-                    class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    type='submit'
+                    class='inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
                     onClick={() => {
                       navigate(`/onecourse/${singlecourse}`);
-                    //   setSinglecourse(course._id._id);
+                      //   setSinglecourse(course._id._id);
                     }}
                   >
                     Details
