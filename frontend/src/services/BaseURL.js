4c0f17295483f6c9f3ac4033f7998f085b4c2fa9
RafayFarrukh <rafayfarrukh941@gmail.com>
2023-04-22 22:05:43 +0500

    vercel fix

--- frontend/src/services/BaseURL.js
+++ frontend/src/services/BaseURL.js
@@ -3,5 +3,5 @@
 //   process.env.NODE_ENV == 'development'
 //     ? 'http://localhost:5000'
 //     : 'https://qr-attendence-system-backend.vercel.app';
-const baseURL = 'https://qr-attendence-system-chi.vercel.app';
+const baseURL = 'https://qr-attendence-system-backend.vercel.app';
 export default baseURL;
