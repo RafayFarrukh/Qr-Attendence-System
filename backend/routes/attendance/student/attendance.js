1620d93f5f27b04ee0c926d3bb5c911cb6c7121a
RafayFarrukh <ammarasif509@gmail.com>
2023-04-29 05:39:43 +0500

    small update in attendance

--- backend/routes/attendance/student/attendance.js
+++ backend/routes/attendance/student/attendance.js
@@ -8,69 +8,57 @@ const Course = require('../../../models/Course');
 const Class = require('../../../models/Class');
 const Attendance = require('../../../models/Attendance');
 
-router.get('/calculateAttendance/:classId/students/:studentId', async function (req, res, next) {
-  try {
-    // Find the class by ID
-    const classId = req.params.classId;
-    const classInfo = await Class.findById(classId).exec();
+router.get(
+  '/calculateAttendance/:classId/students/:studentId',
+  async function (req, res, next) {
+    try {
+      // Find the class by ID
+      const classId = req.params.classId;
+      const classInfo = await Class.findById(classId).exec();
 
-    // Make sure the class exists
-    if (!classInfo) {
-      return res.status(404).json({ message: 'Class not found' });
+      // Make sure the class exists
+      if (!classInfo) {
+        return res.status(404).json({ message: 'Class not found' });
+      }
+
+      // Find the student by ID
+      const student = await Student.findById(req.params.studentId).exec();
+
+      // Make sure the student exists
+      if (!student) {
+        return res.status(404).json({ message: 'Student not found' });
+      }
+
+      // Calculate the student's attendance for the class
+      const attendances = await Attendance.find({
+        ClassId: classId,
+        stdId: student.stdId,
+      }).exec();
+      const totalClasses = await Attendance.find({ ClassId: classId })
+        .distinct('date')
+        .exec();
+      const attendedClasses = attendances.length;
+      const attendancePercentage =
+        (attendedClasses / totalClasses.length) * 100;
+      // const totalClasses = await Attendance.find({ ClassId: classId }).distinct('date').exec();
+      // console.log(classInfo,"Class Info")
+      // let attendedClasses = 0;
+
+      console.log(attendedClasses, ' attendedClasses');
+      // const attendancePercentage = (attendedClasses / totalClasses) * 100;
+
+      return res.status(200).json({
+        message: 'Attendance fetched successfully',
+        attendancePercentage,
+        totalClasses: totalClasses.length,
+        attendedClasses,
+        success: true,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
     }
-
-    // Find the student by ID
-    const student = await Student.findById(req.params.studentId).exec();
-      
-
-    // Make sure the student exists
-    if (!student) {
-      return res.status(404).json({ message: 'Student not found' });
-    }
-
-    // Calculate the student's attendance for the class
-     const attendances = await Attendance.find({ ClassId: classId, stdId: student.stdId }).exec();
-    const totalClasses = await Attendance.find({ ClassId: classId }).distinct('date').exec();
-    const attendedClasses = attendances.length;
-    const attendancePercentage = (attendedClasses / totalClasses.length) * 100;
-    // const totalClasses = await Attendance.find({ ClassId: classId }).distinct('date').exec();
-    // console.log(classInfo,"Class Info")
-    // let attendedClasses = 0;
-
-
-     console.log(attendedClasses," attendedClasses")
-    // const attendancePercentage = (attendedClasses / totalClasses) * 100;
-
-    return res.status(200).json({
-      message: 'Attendance fetched successfully',
-      attendancePercentage,
-      success: true,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  },
+);
 
 module.exports = router;
