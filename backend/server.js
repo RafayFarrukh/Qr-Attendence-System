3edf73f1b9679fcbca06f945dc936aac52e9b3a7
AmmarAsif <ammarasif509@gmail.com>
2023-06-11 04:35:06 +0500

    Cors bug

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,18 @@ const studentAttendance = require('./routes/attendance/student/attendance.js');
 require('dotenv').config();
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 //path
 //auth
